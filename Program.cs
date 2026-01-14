5ca954837f88d0a6a120fded7a3556724d886882
rainyears <rainyrrayne@gmail.com>
2026-01-14 10:06:30 +0700

    Menambahkan input nama dan nilai

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿ss
+﻿string namaSiswa;
+int nilaiSiswa;
+
+Console.Write("Aplikasi Nilai Siswa");
+
+Console.Write("\nMasukkan Nama Siswa: ");
+namaSiswa = Console.ReadLine();
+
+Console.Write("Masukkan Nilai Siswa: ");
+nilaiSiswa = int.Parse(Console.ReadLine());
